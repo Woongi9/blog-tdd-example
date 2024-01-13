3f5fc557341430c0e2c5b3528615ce4357352b14
Woong <woongi9@naver.com>
2024-01-14 03:27:55 +0900

    [INIT] - project

---
+++ src/main/java/com/example/blogtddexample/BlogTddExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogtddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTddExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogtddexample/BlogTddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogtddexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTddExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
